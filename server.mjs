0244dc51a6e1e3a1cdf8d2b3819ef223e831447e
aleifericsson <alifnafili@gmail.com>
2023-10-28 18:06:01 +0100

    handle chart.js created

--- server.mjs
+++ server.mjs
@@ -75,10 +75,11 @@ app.get('/api/accounts/:accountID', async (req, res) => {
 app.get('/api/transactions/:accountID', async (req, res) => {
     try {
         const accountID = req.params.accountID; // Get the accountID from the URL
-        const authJWT = 'YOUR_AUTH_JWT'; // Replace with your actual JWT token
+        const authJWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2OTYwMzIwMDAsImFwaV9zdWIiOiJjMjliMTVjNzZmY2E4N2NiNTMzM2MzMmM4NzY0YWY0MmJjMGUxNjBkMzU4YzZlZDE1MGJjZjZmZWZiNjQzMmE5MTcxNzIwMDAwMDAwMCIsInBsYyI6IjVkY2VjNzRhZTk3NzAxMGUwM2FkNjQ5NSIsImV4cCI6MTcxNzIwMDAwMCwiZGV2ZWxvcGVyX2lkIjoiYzI5YjE1Yzc2ZmNhODdjYjUzMzNjMzJjODc2NGFmNDJiYzBlMTYwZDM1OGM2ZWQxNTBiY2Y2ZmVmYjY0MzJhOSJ9.lx7zs8uJa9s73nnnZS8dXYmYUQPhhZ4sdd7_HdU0MmSBRuFa2DrJD2EnMnAyjIAjuGV7DpsLKL4gIA5UGmzK0btPb9tcmpffYg05xe3dtbGyaG-gStLSiSPblVwM3VbAFt0Eo6rGoQy6MDmwo9WgT8X_ty00MoYUbaPOUG6lIDeXXT6rlUkqQGmSJjoR8b8b0yHY1ONguH66LdrtipPXkSsFS-1h-kGA0q-ljNmNF-pXz4k7Js5rKFY4FaEVtUJbjM2JJH3ftcVV0vvpIjW37q-oL_msD6VS7aiJ5sECbocDeeOAH_xI07U6JhcL1O4rh1wx5T9izce0K4m9HPi8zg'; // Replace with your actual JWT token
+
 
         // Define the URL for the GET request with query parameters
-        const apiUrl = `https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/transactions/accounts/${accountID}/transactions?status=eq:Successful&amount=gte:12.34&lt:43.21`;
+        const apiUrl = `https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/transactions/accounts/${accountID}/transactions`;
 
         // Make the GET request to the Capital One API
         const response = await fetch(apiUrl, {
